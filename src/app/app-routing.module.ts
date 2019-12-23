793c7ded93ffa7dcb8a1927a45f2e9f8a10cd6b2
Bytes Developer <developers@bytestechnolab.in>
2019-12-23 11:53:44 +0530

    Add new component for display commits

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { CommitsComponent } from './commits/commits.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'commits', component: CommitsComponent },
+  { path: '',
+    redirectTo: '/commits',
+    pathMatch: 'full'
+  },
+  { path: '**', component: CommitsComponent }
+];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routedComponents = [
+  CommitsComponent,
+]
