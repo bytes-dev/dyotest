1263f265f0a8f12c34b3a60a1815134fdd4acb69
Bytes Developer <developers@bytestechnolab.in>
2019-12-23 12:12:29 +0530

    Change project structure and groups component in single folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { CommitsComponent } from './commits/commits.component';
+import { CommitsComponent } from './components/commits/commits.component';
 
 const routes: Routes = [
   { path: 'commits', component: CommitsComponent },
