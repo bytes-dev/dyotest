0e4cd9db06c4e4039ac3995691f2d8c43d5361e9
Bytes Developer <developers@bytestechnolab.in>
2019-12-23 14:07:41 +0530

    set default orgnization details in local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 
+import { LocalStorageService } from './core/services/local-storage.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(
+    private localStorageService: LocalStorageService,
+  ) {
+    this.localStorageService.setDefault();
+  }
 }
