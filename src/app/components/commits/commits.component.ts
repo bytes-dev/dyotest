ace5afed8f067ebc67a4ff09455c0286fbaf28a4
Bytes Developer <developers@bytestechnolab.in>
2019-12-23 18:54:32 +0530

    add css and implement group by functionality

--- src/app/components/commits/commits.component.ts
+++ src/app/components/commits/commits.component.ts
@@ -2,6 +2,7 @@ import { LocalStorageService } from './../../core/services/local-storage.service
 import { CommitsService } from './../../core/http-service/commits.service';
 import { Component, OnInit } from '@angular/core';
 import { BranchsService } from 'src/app/core/http-service';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-commits',
@@ -9,6 +10,10 @@ import { BranchsService } from 'src/app/core/http-service';
   styleUrls: ['./commits.component.css']
 })
 export class CommitsComponent implements OnInit {
+  Utils = {
+    keys: Object.keys,
+  };
+  isLoaded = false;
   filters = {
     page: 1,
     per_page: 100,
@@ -16,10 +21,12 @@ export class CommitsComponent implements OnInit {
   }
   commits: any = [];
   branches: any = [];
+  groupCommits: any;
   organisationData = {
     organisation: '',
     repository: ''
   }
+  isError: boolean;
 
   constructor(
     private commitsService: CommitsService,
@@ -33,20 +40,34 @@ export class CommitsComponent implements OnInit {
   }
 
   getCommits(isLocalStorageNeedUpdate) {
+    this.isLoaded = true;
     this.commitsService.getAll(this.filters, this.organisationData).subscribe((data) =>{
+      this.isLoaded = false;
       this.commits = data;
+      this.commits.forEach(commit => {
+        commit.groupDate = (commit.commit.committer.date || '').split("T")[0]
+      });
+      this.groupCommits = _.groupBy(this.commits, 'groupDate');
       if (isLocalStorageNeedUpdate) {
         this.localStorageService.set(this.organisationData);
       }
+    }, (error) => {
+      this.isLoaded = false;
     })
   }
 
   getBranches(isLocalStorageNeedUpdate) {
+    this.isLoaded = true;
     this.branchsService.getAll(this.organisationData).subscribe((data) =>{
       this.branches = data;
       const masterBranchData = this.branches.find((branch) => branch.name === 'master');
       this.filters.sha = masterBranchData.commit.sha;
       this.getCommits(isLocalStorageNeedUpdate);
+    }, (error) => {
+      this.commits = [];
+      this.branches = [];
+      this.isLoaded = false;
+      this.isError = true;
     })
   }
 
